67bc1c0ccb2e16978d2d6860a6f10f5c6b966987
Josh Amore <joshamore31@gmail.com>
2020-02-17 21:11:22 +1100

    added modules and API auth

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+'use strict'
+
 const express = require('express');
+const snoowrap = require('snoowrap');
+
 require('dotenv').config()
 
+const reddit = new snoowrap({
+    userAgent: process.env.R_USER_AGENT,
+    clientId: process.env.R_CLIENT_ID,
+    clientSecret: process.env.R_SECRET,
+    username: process.env.R_USERNAME,
+    password: process.env.R_PASSWORD
+  });
+
+reddit.getHot().map(post => post.title).then(console.log);
