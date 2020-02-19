46e45b5d416f3f6972b8aa1ad12b2561a6daa6a3
Josh Amore <joshamore31@gmail.com>
2020-02-19 17:33:45 +1100

    added testing functions to render returned data to page

---
+++ static/scripts.js
@@ -0,0 +1,28 @@
+// AJAX call for data
+function getData() {
+    const xhr = new XMLHttpRequest();
+
+    // Setup listener to process completed requests
+    xhr.onload = () => {
+        // Process returned data
+        if (xhr.status >= 200 && xhr.status < 300) {
+            console.log('success!', xhr);
+            dataRender(xhr.response);
+        } else {
+            console.log('The request failed!');
+        }
+    };
+
+    xhr.open('GET', 'http://localhost:5000/data');
+    xhr.send();
+}
+
+// Renders data
+function dataRender(redditData) {
+    document.getElementById("getSubData").innerHTML = redditData;
+}
+
+// Click event
+onload = () => {
+    document.getElementById("getSubData").addEventListener("click", getData());
+}
