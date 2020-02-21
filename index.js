d5d01aaccf25c2cc748457ef01fd0926577bda74
Josh Amore <joshamore31@gmail.com>
2020-02-21 18:17:41 +1100

    moved routes to dedicated dir

--- index.js
+++ index.js
@@ -2,52 +2,17 @@
 
 const express = require('express');
 const path = require('path');
-const snoowrap = require('snoowrap');
-require('dotenv').config()
 
-// Authorising reddit script
-const reddit = new snoowrap({
-    userAgent: process.env.R_USER_AGENT,
-    clientId: process.env.R_CLIENT_ID,
-    clientSecret: process.env.R_SECRET,
-    username: process.env.R_USERNAME,
-    password: process.env.R_PASSWORD
-});
+require('dotenv').config()
 
 // Creates express app
 const app = express();
 
+// Pull in routes file.
+app.use('/api/', require('./routes/api/dataCalls'));
+
 // Serves static files
 app.use(express.static('static'))
-
-// Home route
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/static/index.html'))
-});
-
-// data route
-app.get('/data', (req, res) => {
-  // Getting promise of specified reddit user
-  const person = reddit.getUser(process.env.REDDIT_PERSON);
-  // Getting submissions for reddit user.
-  person.getSubmissions({limit: 60}).then((content) => {
-    // Printing title of submissions and score
-    let userRedditData = {
-      titles: [],
-      subreddits: [],
-      scores: []
-    };
-
-    // Pushing elements to object.
-    for (let i = 0; i < content.length; i++) {
-      userRedditData.titles.push(content[i].title);
-      userRedditData.subreddits.push(content[i].subreddit);
-      userRedditData.scores.push(content[i].score);
-    }
-
-    res.json(userRedditData);
-  }); 
-});
 
 // Uses port from environment or 500 if none.
 const PORT = process.env.PORT || 5000;
---
+++ routes/api/dataCalls.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const snoowrap = require('snoowrap');
+
+
+// Authorising reddit script
+const reddit = new snoowrap({
+    userAgent: process.env.R_USER_AGENT,
+    clientId: process.env.R_CLIENT_ID,
+    clientSecret: process.env.R_SECRET,
+    username: process.env.R_USERNAME,
+    password: process.env.R_PASSWORD
+});
+
+// Home route
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/static/index.html'))
+});
+  
+// data route
+router.get('/data', (req, res) => {
+    // Getting promise of specified reddit user
+    const person = reddit.getUser(process.env.REDDIT_PERSON);
+
+    // Getting submissions for reddit user.
+    person.getSubmissions({limit: 60}).then((content) => {
+        // Printing title of submissions and score
+        let userRedditData = {
+            titles: [],
+            subreddits: [],
+            scores: []
+        };
+
+        // Pushing elements to object.
+        for (let i = 0; i < content.length; i++) {
+            userRedditData.titles.push(content[i].title);
+            userRedditData.subreddits.push(content[i].subreddit);
+            userRedditData.scores.push(content[i].score);
+        }
+
+        // Return reddit data
+        res.json(userRedditData);
+    }); 
+});
+
+// Exporting module
+module.exports = router;
