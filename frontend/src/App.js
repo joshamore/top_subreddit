8de41398c8e375a7f3a59bce86ca2adb82250c4f
Josh Amore <joshamore31@gmail.com>
2020-05-06 16:52:03 +1000

    added input field

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./App.css";
 import Appbar from "./components/Appbar";
+import TextField from "@material-ui/core/TextField";
 
 function App() {
 	return (
 		<div className="App">
 			<Appbar />
-			<h1>Top Subreddit</h1>
+			<h1 className="addUserHeading">Enter a Reddit username</h1>
+			<form noValidate autoComplete="off">
+				<TextField
+					id="outlined-basic"
+					label="Reddit Username"
+					variant="outlined"
+				/>
+			</form>
 		</div>
 	);
 }
