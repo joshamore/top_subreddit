4b82e7e633ed12aecf41a51ee3d9e288de731512
Josh Amore <joshamore31@gmail.com>
2020-05-06 19:02:54 +1000

    added Subreddits component (will render results)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,13 @@
 import React from "react";
 import "./App.css";
 import Appbar from "./components/Appbar";
-import TextField from "@material-ui/core/TextField";
+import GetUsername from "./components/GetUsername";
 
 function App() {
 	return (
 		<div className="App">
 			<Appbar />
-			<h1 className="addUserHeading">Enter a Reddit username</h1>
-			<form noValidate autoComplete="off">
-				<TextField
-					id="outlined-basic"
-					label="Reddit Username"
-					variant="outlined"
-				/>
-			</form>
+			<GetUsername />
 		</div>
 	);
 }
---
+++ frontend/src/components/Subreddits.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import { FormGroup } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+export default function Subreddits() {
+	return (
+		<React.Fragment>
+			<h1>Subreddits</h1>
+		</React.Fragment>
+	);
+}
