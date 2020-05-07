12b23cebcd8f7ac480b8c7d1dd11f8622b7e76fb
Josh Amore <joshamore31@gmail.com>
2020-05-07 16:32:43 +1000

    added CORS middleware

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-'use strict'
-const express = require('express');
-const path = require('path');
+"use strict";
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
 
-require('dotenv').config()
+require("dotenv").config();
 
 // Creates express app
 const app = express();
 
+// Enabling CORS middleware
+app.use(cors());
+
 // Pull in routes file.
-app.use('/api/', require('./routes/api/userCalls'));
+app.use("/api/", require("./routes/api/userCalls"));
 
 // Serves static files
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // Uses port from environment or 500 if none.
 const PORT = process.env.PORT || 5000;
