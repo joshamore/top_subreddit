a792f85c3f2e1ab0d7635b365805bf7c56347cee
Josh Amore <joshamore31@gmail.com>
2020-05-08 12:57:46 +1000

    added subredditTable component (empty ATM)

---
+++ frontend/src/components/SubredditTable.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+export default function SubredditTable() {
+	return (
+		<React.Fragment>
+			<p>TODO: add table</p>
+		</React.Fragment>
+	);
+}
--- frontend/src/components/Subreddits.js
+++ frontend/src/components/Subreddits.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import SubredditTable from "./SubredditTable";
 
 export default function Subreddits({ userHistory }) {
-	console.log(userHistory);
+	const useStyles = makeStyles((theme) => ({
+		sHeading: {
+			textAlign: "center",
+		},
+	}));
+	const classes = useStyles();
+
 	return (
 		<React.Fragment>
-			<h1>Subreddits</h1>
-			<p>{userHistory}</p>
+			<h1 className={classes.sHeading}>Subreddits</h1>
+			<SubredditTable />
 		</React.Fragment>
 	);
 }
