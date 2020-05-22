963778f5184d10e066af80cd11c62d0b436e7a2a
Josh Amore <joshamore31@gmail.com>
2020-05-22 14:03:06 +1000

    added link to code in navbar

--- frontend/src/components/Appbar.js
+++ frontend/src/components/Appbar.js
@@ -3,6 +3,8 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import GitHubIcon from "@material-ui/icons/GitHub";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -26,6 +28,14 @@ export default function ButtonAppBar() {
 					<Typography variant="h6" className={classes.title}>
 						Top Subreddits
 					</Typography>
+					<Button color="inherit">
+						<GitHubIcon
+							onClick={(event) =>
+								(window.location.href =
+									"https://github.com/joshamore/top_subreddit")
+							}
+						/>
+					</Button>
 				</Toolbar>
 			</AppBar>
 		</div>
