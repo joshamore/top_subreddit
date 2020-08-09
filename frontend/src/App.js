fec9242329c3bdf824ae95458821682b41c2c751
Josh Amore <joshamore31@gmail.com>
2020-08-09 10:27:11 +1000

    Updated theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Appbar from "./components/Appbar";
 import GetUsername from "./components/GetUsername";
 import Subreddits from "./components/Subreddits";
+
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: "#880E4F",
+		},
+		secondary: {
+			main: "#0E8847",
+		},
+	},
+});
 
 function App() {
 	// declaring state hooks
 	const [userHistory, setUserHistory] = useState([]);
 	const [redditUser, setRedditUser] = useState("");
 	const [gotUser, setGotUser] = useState(false);
+
+	// Pinging heroku to warmup server on pageload
+	useEffect(() => {
+		fetch("https://topsubreddit.herokuapp.com/api/ping")
+			.then((res) => console.log("ping"))
+			.catch((e) => console.log(e));
+	}, []);
 
 	const updateUserHistory = (history, user) => {
 		setUserHistory(history);
@@ -17,24 +37,34 @@ function App() {
 	};
 
 	const nextUser = () => {
-		console.log("HERE");
 		setGotUser(false);
 	};
 
-	return (
-		<div className="App">
-			<Appbar />
-			{gotUser ? (
-				<Subreddits
-					userHistory={userHistory}
-					redditUser={redditUser}
-					nextUser={nextUser}
-				/>
-			) : (
-				<GetUsername updateUserHistory={updateUserHistory} />
-			)}
-		</div>
-	);
+	if (gotUser) {
+		return (
+			<MuiThemeProvider theme={theme}>
+				<div className="App">
+					<Appbar />
+					<Subreddits
+						userHistory={userHistory}
+						redditUser={redditUser}
+						nextUser={nextUser}
+					/>
+				</div>
+			</MuiThemeProvider>
+		);
+	} else if (!gotUser) {
+		return (
+			<MuiThemeProvider theme={theme}>
+				<div className="App">
+					<Appbar />
+					<div>
+						<GetUsername updateUserHistory={updateUserHistory} />
+					</div>
+				</div>
+			</MuiThemeProvider>
+		);
+	}
 }
 
 export default App;
